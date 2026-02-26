fba1a4dd3ee6fd6941c280fb3b6637be73a218c6
elliotced <elliotceder@gmail.com>
2026-02-26 16:45:47 +0100

    Login
    2026-02-26

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'debug'
 require "awesome_print"
+require 'sinatra'
+require 'securerandom'
 
 class App < Sinatra::Base
 
@@ -15,9 +17,68 @@ class App < Sinatra::Base
       return @db
     end
 
+    before do
+        @current_page = ""
+    end
+
+    
+
     # Routen /
     get '/' do
-        erb :index
+        @current_page = "Home"
+        erb :"index"
+    end
+
+    get '/users' do
+        @current_page = "Users"
+        erb :"users/user"
+    end
+
+    get '/users/login' do
+        @current_page = "Login"
+        erb :"users/login"
+    end
+
+    post '/users/login' do
+        request_username = params[:username]
+        request_plain_password = params[:password]
+
+        user = db.execute("SELECT *
+                FROM users
+                WHERE username = ?",
+                request_username).first
+
+        unless user
+            ap "/users/login : Invalid username."
+            status 401
+            redirect '/acces_denied'
+        end
+
+        db_id = user["id"].to_i
+        db_password_hashed = user["password"].to_s
+
+        # Create a BCrypt object from the hashed password from db
+        bcrypt_db_password = BCrypt::Password.new(db_password_hashed)
+        # Check if the plain password matches the hashed password from db
+        if bcrypt_db_password == request_plain_password
+            ap "/users/login : Logged in -> redirecting to admin"
+            session[:user_id] = db_id
+            redirect '/admin'
+        else
+            ap "/users/login : Invalid password."
+            status 401
+            redirect '/acces_denied'
+        end
+    end
+
+    get '/users/register' do
+        @current_page = "Register"
+        erb :"users/register"
+    end
+
+    get '/acces_denied' do
+        @current_page = "401"
+        erb :"access_denied"
     end
 
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,4 +1,6 @@
 require 'sqlite3'
+require_relative '../config'
+require 'bcrypt'
 
 class Seeder
   
@@ -9,20 +11,21 @@ class Seeder
   end
 
   def self.drop_tables
-    db.execute('DROP TABLE IF EXISTS genres')
+    db.execute('DROP TABLE IF EXISTS users')
     db.execute('DROP TABLE IF EXISTS artists')
     db.execute('DROP TABLE IF EXISTS artist_genres')
     db.execute('DROP TABLE IF EXISTS albums')
     db.execute('DROP TABLE IF EXISTS album_genres')
     db.execute('DROP TABLE IF EXISTS songs')
     db.execute('DROP TABLE IF EXISTS song_genres')
+    db.execute('DROP TABLE IF EXISTS genres')
   end
 
   def self.create_tables
-    db.execute('CREATE TABLE genres (
+    db.execute('CREATE TABLE users (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT,
-                png_path TEXT)')
+                username TEXT NOT NULL,
+                password TEXT NOT NULL)')
 
     db.execute('CREATE TABLE artists (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -54,9 +57,18 @@ class Seeder
     db.execute('CREATE TABLE song_genres (
                 genre_id INTEGER,
                 song_id INTEGER)')
+
+    db.execute('CREATE TABLE genres (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                png_path TEXT)')
   end
 
   def self.populate_tables
+    password_hashed = BCrypt::Password.create("123")
+    p "Storing hashed password (#{password_hashed}) to DB. Clear text password (123) never saved."
+    db.execute('INSERT INTO users (username, password) VALUES (?, ?)', ["ola", password_hashed])
+
     db.execute('INSERT INTO artists (name, description) VALUES ("Tame Impala", "Kevin parker is from Australia.")')
     db.execute('INSERT INTO songs (name, mp3_path, artist_id) VALUES ("Borderline", "songs/borderline.mp3", 1)')
   end
